c42f1a887354b4d674b1caa9dc26050888b4b4d4
carlowil <matveysizov84@gmail.com>
2023-04-11 19:12:18 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:2.2.4")
+    implementation("io.ktor:ktor-server-netty:2.2.4")
+    implementation("io.ktor:ktor-server-websockets:2.2.4")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
+    testImplementation("io.ktor:ktor-server-test-host:2.2.4")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.20")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("Application")
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.carlowil
+
+
+import io.ktor.server.application.*
+
+
+fun main(args:Array<String>) : Unit {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+    configureSockets()
+}
